50b1ab4cf7f929aa26a6c1962889edc69f734121
dodekoekjes <dodekoekjes@gmail.com>
2018-09-25 02:19:58 +0200

    added a sort of Main menu

--- Properties/PlayButton.cs
+++ Properties/PlayButton.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public class Panel : Godot.Panel
+public class PlayButton : MenuButton
 {
     // Member variables here, example:
     // private int a = 2;
@@ -11,19 +11,23 @@ public class Panel : Godot.Panel
     {
         // Called every time the node is added to the scene.
         // Initialization here
-        GetNode("Button").Connect("pressed", this, nameof(_OnButtonPressed));
+		var popup = GetPopup();
+		popup.AddItem("item a");
+	    popup.AddItem("item b");
+	    popup.AddItem("item c");
+	    popup.Connect("id_pressed", this, "OnItemPressed");
+	}
+
+	private void OnItemPressed(string id) {
+	    var label = (Label)GetNode("../Label");
+	    label.Text = id;
+		GD.Print("blah.");
     }
 
 //    public override void _Process(float delta)
 //    {
 //        // Called every frame. Delta is time since last frame.
 //        // Update game logic here.
-//        
+//
 //    }
-
-	public void _OnButtonPressed()
-	{
-	    var label = (Label)GetNode("Label");
-	    label.Text = "HELLO!";
-	}
 }
